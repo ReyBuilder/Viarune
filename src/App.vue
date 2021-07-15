8a4215eca8e2b35bf8185af0ef944b5c2deac97f
Alexander <@.>
2021-07-15 17:59:01 +0300

    third part ready 1

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,21 @@
 <template>
   <div>
-    <intro></intro>
+    <Intro></Intro>
     <About_school></About_school>
-    <div class="our_subjects">
-      <div class="rudiment">
-        <img src="./components/assets/images/rudiment.svg" alt="">
-      </div>
-      <div class="subj__container">
-        <div class="subj__title">
-          Наши предметы
-        </div>
-        <div class="subj__filters">
-          <a class="buttonWrapper" href="#">
-            <div class="buttonBoxFilter buttonFilter">
-              Все
-            </div>
-          </a>
-          <a class="buttonWrapper" href="#">
-            <div class="buttonBoxFilter buttonFilter">
-              Программирование
-            </div>
-          </a>
-          <a class="buttonWrapper" href="#">
-            <div class="buttonBoxFilter buttonFilter">
-              Дизайн
-            </div>
-          </a>
-          <a class="buttonWrapper" href="#">
-            <div class="buttonBoxFilter buttonFilter">
-              Иностранные языки
-            </div>
-          </a>
-          <a class="buttonWrapper" href="#">
-            <div class="buttonBoxFilter buttonFilter">
-              Школьные предметы
-            </div>
-          </a>
-        </div>
-        <div class="subj_cards"></div>
-      </div>
-    </div>
+    <About_subj></About_subj>
   </div>
 </template>
 <script>
 import Intro from "./components/Intro.vue";
 import About_school from "./components/Second_part.vue";
+import About_subj from "./components/Third_part.vue";
 
 export default {
   name: "App",
   components: {
     Intro,
     About_school,
+    About_subj,
   },
   created() {
     document.title = "Viarune study";
@@ -60,65 +25,4 @@ export default {
 <style>
 @import url(~@/assets/fonts/font_gilroy.css);
 @import url(~@/assets/BaseStyles.css);
-</style>
-<style scoped>
-/* Our Subjects */
-.our_subjects {
-  padding-top: 250px;
-  position: relative;
-}
-
-.rudiment{
-  position: absolute;
-  top: -200px;
-  z-index: -1;
-}
-.subj__container {
-  width: 100%;
-  max-width: 1000px;
-  margin: 0 auto;
-}
-
-.subj__title {
-  text-align: center;
-  font-weight: 800;
-  font-size: 44px;
-  line-height: 54px;
-
-  color: #30203d;
-
-  margin-bottom: 39px;
-}
-
-/* Filters */
-.subj__filters{
-  display: flex;
-  justify-content: space-between;
-}
-
-.buttonBoxFilter {
-  font-family: Roboto;
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 16px;
-  text-align: center;
-  color: #4E3D95;
-  text-decoration: none;
-  text-align: center;
-
-  cursor: pointer;
-  margin: 0;
-  padding: 0;
-  border-radius: 50px;
-  border: 1px solid #4E3D95;;
-
-  display: inline-block;
-  vertical-align: top;
-
-  width: 100%;
-
-  padding: 18px 40px 21px;
-  border-radius: 50px;
-}
-
 </style>
---
+++ src/components/Third_part.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <div class="our_subjects">
+      <div class="rudiment">
+        <img src="./assets/images/rudiment.svg" alt="" />
+      </div>
+      <div class="subj__container">
+        <div class="subj__title">
+          Наши предметы
+        </div>
+        <div class="subj__filters">
+          <a class="buttonWrapper" href="#">
+            <div class="buttonBoxFilter buttonFilter">
+              Все
+            </div>
+          </a>
+          <a class="buttonWrapper" href="#">
+            <div class="buttonBoxFilter buttonFilter">
+              Программирование
+            </div>
+          </a>
+          <a class="buttonWrapper" href="#">
+            <div class="buttonBoxFilter buttonFilter">
+              Дизайн
+            </div>
+          </a>
+          <a class="buttonWrapper" href="#">
+            <div class="buttonBoxFilter buttonFilter">
+              Иностранные языки
+            </div>
+          </a>
+          <a class="buttonWrapper" href="#">
+            <div class="buttonBoxFilter buttonFilter">
+              Школьные предметы
+            </div>
+          </a>
+        </div>
+        <div class="subj__cards">
+          <div class="subj_cards__inner">
+            <div class="subj__card">
+              <div class="card__header">
+                <div class="card__title">
+                    Язык программирования: Python
+                </div>
+                <div class="card__logo">
+                    <img
+                      src="./assets/images/subj_cards/python-logo2.svg" alt=""/>
+                </div>
+              </div>
+              <div class="card__text">
+                  Научим вас и вашего ребенка кодить на высокоуровневом языке программирования. Без слез...
+              </div>
+              <div class="card__underground">
+                  <a class="buttonWrapper" href="#">
+                    <div class="buttonBox buttonPurple">
+                        Записаться
+                    </div>
+                  </a>
+                  <a class="" href="#">
+                    <div class="">
+                        Подробнее
+                    </div>
+                  </a>
+              </div>
+            </div>
+            <div class="subj__card">2</div>
+            <div class="subj__card">3</div>
+          </div>
+          <div class="subj_cards__inner"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Our Subjects */
+.our_subjects {
+  padding-top: 250px;
+  position: relative;
+}
+
+.rudiment {
+  position: absolute;
+  top: -200px;
+  z-index: -1;
+}
+.subj__container {
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.subj__title {
+  text-align: center;
+  font-weight: 800;
+  font-size: 44px;
+  line-height: 54px;
+
+  color: #30203d;
+
+  margin-bottom: 39px;
+}
+
+/* Filters */
+.subj__filters {
+  display: flex;
+  justify-content: space-between;
+}
+
+.buttonBoxFilter {
+  font-family: Roboto;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+  color: #4e3d95;
+  text-decoration: none;
+  text-align: center;
+
+  cursor: pointer;
+  margin: 0;
+  padding: 0;
+  border-radius: 50px;
+  border: 3px solid #4e3d95;
+
+  display: inline-block;
+  vertical-align: top;
+
+  width: 100%;
+
+  padding: 18px 40px 21px;
+  border-radius: 50px;
+}
+
+/* Cards Containers */
+.subj__cards {
+  padding-top: 40px;
+  padding-bottom: 75px;
+}
+.subj_cards__inner {
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+}
+/* Card */
+.subj__card {
+  /* width: 100%; */
+  max-width: 320px;
+  background: #ffffff;
+  box-shadow: 4px 4px 20px rgba(48, 32, 61, 0.1);
+  border-radius: 6px;
+  padding: 20px 20px 30px;
+}
+
+.card__header {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.card__title{
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 150%;
+
+    padding-top: 10px;
+    max-width: 175px;
+    text-align: left;
+}
+
+.card__text{
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+
+    color: #30203D;
+
+    margin-bottom: 20px;
+}
+
+.card__underground{
+    display: flex;
+    position: relative;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 10px;
+}
+
+.buttonBox {
+  padding: 18px 36px;
+  border-radius: 40px;
+}
+/* .buttonWrapper {
+  max-width: 160px;
+} */
+</style>
