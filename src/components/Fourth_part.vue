3bc5b8f181c7c25c1cb7b6e7766637a6a2197333
Alexander <@.>
2021-07-18 17:54:50 +0300

    попытка 3

--- src/components/Fourth_part.vue
+++ src/components/Fourth_part.vue
@@ -17,14 +17,21 @@
       </div>
       <div class="appointment__content">
         <div class="appointment_input__form">
+            <div class="form__item">
+           <input class="inputCustom" href="#">
+        </div>
           <div class="form__item">
-            <input class="inputWrapper" type="text" />
+            <input class="inputCustom" type="text" />
           </div>
           <div class="form__item">
-            <input type="text" />
+            <input class="inputCustom" type="text" />
           </div>
-          <div class="form__item">
-            <input type="text" />
+          <div class="form_item__checkbox form__item">
+              <input type="checkbox" id="subscribe" name="subscribe" value="newsletter">
+              <label for="subscribe">Я согласен с условиями обработки <br> персональных данных</label>
+          </div>
+          <div>
+              <a class="buttonBox buttonOrange" href="#">Записаться</a>
           </div>
         </div>
       </div>
@@ -32,35 +39,50 @@
   </div>
 </template>
 <style scoped>
+
+.buttonBox {
+  padding: 18px 125px;
+  border-radius: 30px;
+}
+
+.buttonBox:focus{
+    outline-offset: 3px;
+    outline: 1px solid #30203D;
+}
 /* Main part & background */
 .appointment__main {
   position: relative;
   width: 100%;
-  max-height: 1046px;
-  margin-bottom: 364px;
+  /* max-height: 1046px; */
 }
 
 .appointment__bg img {
   width: 100%;
   object-fit: cover;
+  position: absolute;
+  z-index: -1;
+  /* max-height: 1046px; */
 }
 
 .appointment__content {
   top: 0;
-  bottom: 0;
-  position: absolute;
-  margin: 364px auto 274px;
+  /* bottom: 0; */
+  /* position: absolute; */
+  margin: 35% auto 26%;
+
+  width: 100%;
+  max-width: 405px;;
 }
 
 .appointment__header {
   top: 0;
-  bottom: 0;
+  /* bottom: 0; */
   right: 0;
   left: 0;
-  position: absolute;
+  /* position: absolute; */
   margin: 0 auto;
 
-  margin-top: 200px;
+  margin-top: 250px;
   text-align: center;
   color: #fff;
 }
@@ -78,37 +100,66 @@
   line-height: 150%;
   margin: 0 auto;
 }
+
 /* Input form */
 .appointment_input__form {
   background: #fefcff;
   box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.1);
   border-radius: 6px;
   padding: 40px 34px;
-  /* margin-left: 337px; */
-
-  /* height: 408px;
-    width: 405px; */
 }
 .form__item {
   margin-bottom: 20px;
+}
+
+.form_item__checkbox{
+    position: relative;
+    display: flex;
+    align-items: flex-start;
+
+    width: 100%;
+    max-width: 338px;
 }
 
 .container {
   width: 100%;
   margin: 0 auto;
   max-width: 900px;
-  /* position: absolute; */
 }
-/* Input */
-.inputWrapper{
+
+/* Inputs */
+.inputCustom{
     background: #FFFFFF;
     border: 0.5px solid rgba(48, 32, 61, 0.6);
     box-sizing: border-box;
     border-radius: 30px;
+
     font-weight: normal;
     font-size: 14px;
     line-height: 16px;
     padding: 17px 44px 17px 20px;
     color: rgba(48, 32, 61, 0.6);
+
+    width: 100%;
+    max-width: 338px;
 }
+
+.inputCustom:focus{
+    outline-offset: 3px;
+    outline: 1px solid #30203D;
+}
+
+label {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 150%;
+
+    color: rgba(48, 32, 61, 0.6);
+}
+.checkbox{
+    background: #FEFFFF;
+    border: 0.5px solid rgba(48, 32, 61, 0.6);
+    border-radius: 9px;
+}
+
 </style>
