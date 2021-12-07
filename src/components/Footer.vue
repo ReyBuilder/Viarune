0ea08d3f96fbcc39b73d2c04b2181ae285dace10
ReyBuilder <mynameisreybuilder@gmail.com>
2021-12-08 00:16:11 +0300

    footer fixes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,310 +1,190 @@
 <template>
-	<div class="footer__container">
-		<div class="footer__main">
-			<div class="left__part">
-				<div class="title">
-					Свяжитесь с нами
-				</div>
-				<div class="socials">
-					<div class="socials__text">
-						Наши соц.сети:
-					</div>
-					<div class="socials__icons">
-						<a class="icon__wrapper" href="#">
-							<div class="iconBox" id="vk"></div>
-						</a>
-						<a
-							class="icon__wrapper"
-							href="https://www.facebook.com/groups/352779019914924/about"
-						>
-							<div class="iconBox" id="fb"></div>
-						</a>
-						<a
-							class="icon__wrapper"
-							href="https://www.instagram.com/viarunestudy/"
-						>
-							<div class="iconBox" id="inst"></div>
-						</a>
-					</div>
-				</div>
-				<div class="phone__block">
-					<div class="phone__text">
-						По номеру телефона:
-					</div>
-					<div class="phone__number">
-						<div class="phone__icon">
-							<img src="./assets/icons/icon_call.svg" alt="" />
-						</div>
-						<div class="phone__digits">
-							+7 912 013 39-97
-						</div>
-					</div>
-				</div>
-				<div class="socials__icons">
-					<a class="icon__wrapper" href="#">
-						<div class="iconBox" id="tg"></div>
-					</a>
-					<a class="icon__wrapper" href="https://wa.me/79120133997">
-						<div class="iconBox" id="ws"></div>
-					</a>
-					<a class="icon__wrapper" href="#">
-						<div class="iconBox" id="vb"></div>
-					</a>
-				</div>
-			</div>
-			<div class="center__part">
-				<div class="title Docs">
-					Документы
-				</div>
-				<div class="link__wrapper">
-					<a class="underlined__item" href="./Dogovor-oferta.docx"
-						>Договор-оферта</a
-					>
-				</div>
-			</div>
-			<div class="right__part">
-				<div class="title Information">
-					Информация
-				</div>
-				<div class="company_info">
-					ИП "Семакин Александр Александрович"
-				</div>
-				<div class="company_info">ОГРН 321183200030713</div>
-				<div class="company_info">ИНН 182907717333</div>
-			</div>
-		</div>
-	</div>
+    <div class="footer__container">
+        <div class="footer__main">
+            <div class="title">
+                <div class="title__inner">Свяжитесь с нами</div>
+            </div>
+            <div class="contacts">
+                <div class="block">
+                    <div class="block__text">Наши соц.сети:</div>
+                    <div class="block__content">
+                        <a class="icon__wrapper" href="#">
+                            <div class="iconBox" id="vk"></div>
+                        </a>
+                        <a
+                            class="icon__wrapper"
+                            href="https://www.facebook.com/groups/352779019914924/about"
+                        >
+                            <div class="iconBox" id="fb"></div>
+                        </a>
+                        <a
+                            class="icon__wrapper"
+                            href="https://www.instagram.com/viarunestudy/"
+                        >
+                            <div class="iconBox" id="inst"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="block">
+                    <div class="block__text">По номеру телефона:</div>
+                    <div class="block__content">
+                        <div>
+                            <img src="./assets/icons/icon_call.svg" alt="" />
+                        </div>
+                        <div>+7 912 013 39-97</div>
+                    </div>
+                </div>
+                <div class="block">
+                    <div class="block__text">Наши мессенджеры:</div>
+                    <div class="block__content">
+                        <a class="icon__wrapper" href="#">
+                            <div class="iconBox" id="tg"></div>
+                        </a>
+                        <a
+                            class="icon__wrapper"
+                            href="https://wa.me/79120133997"
+                        >
+                            <div class="iconBox" id="ws"></div>
+                        </a>
+                        <a class="icon__wrapper" href="#">
+                            <div class="iconBox" id="vb"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <style scoped>
-@media screen and (max-width:344px) {
-	.footer__container {
-		margin-left: 28px;
-		margin-right: 25px;
-	}
-	.footer__main {
-		justify-content: flex-start;
-		padding-bottom: 22px;
-	}
-	.center__part {
-		margin: 0;
-	}
-}
-@media screen and (min-width: 345px) {
-	.footer__container {
-		margin-left: 28px;
-		margin-right: 25px;
-	}
-	.footer__main {
-		justify-content: flex-start;
-	}
-	.center__part, .left__part {
-		margin-right: 20px;
-	}
-}
-
-@media screen and (min-width: 1123px) {
-	.footer__container {
-		width: 100%;
-		max-width: 1001px;
-		margin: 0 auto;
-	}
-	.right__part {
-		margin-top: 0;
-	}
-	.footer__main {
-		justify-content: space-between;
-		padding-bottom: 54px;
-	}
-}
 .footer__main {
-	padding-top: 77px;
-	position: relative;
-	display: flex;
+    padding-top: 77px;
+    position: relative;
 
-	font-weight: 500;
-	color: #30203d;
-	flex-wrap: wrap;
+    font-weight: 500;
+    color: #30203d;
+}
+
+.block {
+    font-size: 16px;
+}
+
+.block__content {
+    display: flex;
+    align-items: center;
+}
+
+.block__text {
+    margin-bottom: 10px;
+}
+
+.block__content > * {
+    flex-grow: 0;
+    flex-shrink: 0;
 }
 
 .title {
-	font-size: 18px;
-	line-height: 21px;
+    font-size: 18px;
 
-	margin-bottom: 10px;
+    margin-bottom: 38px;
 }
-/* Left Part */
-.left__part {
-	margin-bottom: 30px;
-}
-.socials {
-	margin-bottom: 10px;
-}
-.socials__text {
-	line-height: 19px;
-	margin-bottom: 7px;
-}
-
-/* Иконки непосредственно */
 .icon__wrapper {
-	display: block;
-	border-radius: 3px;
-	margin-right: 10px;
+    display: block;
+    border-radius: 3px;
+    margin-right: 10px;
 
-	width: 32px;
-	height: 32px;
+    width: 32px;
+    height: 32px;
+}
+.icon__wrapper:last-child {
+    margin-right: 0;
 }
 
 .icon__wrapper:hover {
-	box-shadow: 0 0 0 1px rgba(248, 132, 13, 0.9);
+    box-shadow: 0 0 0 1px rgba(248, 132, 13, 0.9);
 }
-
 .icon__wrapper:focus {
-	/* padding: 0; */
-	/* border: #30203d 1px solid; */
-	box-shadow: 0 0 0 1px #30203d, 0 0 0 3px rgba(250, 242, 255),
-		0 0 0 4px #30203d;
+    box-shadow: 0 0 0 1px #30203d, 0 0 0 3px rgba(250, 242, 255),
+        0 0 0 4px #30203d;
 }
 
 .iconBox {
-	width: 32px;
-	height: 32px;
+    width: 32px;
+    height: 32px;
 
-	text-decoration: none;
-	text-align: center;
+    text-decoration: none;
+    text-align: center;
 
-	cursor: pointer;
-	margin: 0;
-	padding: 0;
-	border: none;
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+    border: none;
 
-	display: inline-block;
-	vertical-align: top;
+    display: inline-block;
+    vertical-align: top;
 }
 
 #vk {
-	background: url("./assets/icons/icon_vk.svg") center;
+    background: url("./assets/icons/icon_vk.svg") center;
 }
 
 #fb {
-	background: url("./assets/icons/icon_facebook.svg") center;
+    background: url("./assets/icons/icon_facebook.svg") center;
 }
 
 #inst {
-	background: url("./assets/icons/icon_instagram.svg") center;
+    background: url("./assets/icons/icon_instagram.svg") center;
 }
 
 #tg {
-	background: url("./assets/icons/icon_telegram.svg") center;
+    background: url("./assets/icons/icon_telegram.svg") center;
 }
 
 #ws {
-	background: url("./assets/icons/icon_whatsapp.svg") center;
+    background: url("./assets/icons/icon_whatsapp.svg") center;
 }
 
 #vb {
-	background: url("./assets/icons/icon_viber.svg") center;
+    background: url("./assets/icons/icon_viber.svg") center;
 }
 
-.socials__icon:focus {
-	outline: 1px solid rgba(48, 32, 61, 0.9);
-	outline-offset: 2px;
-	border-radius: 3px;
+@media screen and (max-width: 1124px) {
+    .block {
+        margin-bottom: 16px;
+        margin-left: 38px;
+    }
+    .title {
+        font-family: Gilroy;
+        font-weight: 800;
+        font-style: normal;
+        font-size: 36px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .title__inner {
+        width: 306px;
+        text-align: center;
+    }
+    .footer__main {
+        margin-bottom: 65px;
+    }
 }
 
-.socials_icons__item:hover {
-	padding: 0px;
-	border: 1px solid rgba(248, 132, 13, 0.9);
-	border-radius: 3px;
-}
-
-.socials_icons__item:focus {
-	padding: 2px;
-	border: 1px solid rgba(48, 32, 61, 0.9);
-	border-radius: 3px;
-}
-
-.socials__icons {
-	position: relative;
-	display: flex;
-	justify-content: space-between;
-	max-width: 116px;
-}
-
-/* Phone */
-.phone__block {
-	line-height: 19px;
-	margin-bottom: 10px;
-}
-
-.phone__number {
-	position: relative;
-	display: flex;
-	align-items: center;
-	margin-top: 6px;
-}
-
-.phone__icon {
-	height: 32px;
-}
-
-.phone__digits {
-	padding-left: 3px;
-}
-
-/* Center Part */
-.center__part{
-    margin-bottom: 30px;
-}
-
-.Docs {
-	margin-bottom: 8px;
-}
-
-.link__wrapper {
-	margin-bottom: 10px;
-}
-
-.underlined__item {
-	line-height: 150%;
-	position: relative;
-	text-decoration: none;
-
-	color: #30203d;
-}
-
-.underlined__item:hover {
-	color: #f8840d;
-}
-
-.underlined__item:after {
-	display: block;
-	bottom: 1px;
-	right: 0;
-	content: "";
-	height: 1px;
-	color: #30203d;
-	width: 100%;
-	background-color: #30203d;
-	position: absolute;
-}
-
-.underlined__item:hover:after {
-	background-color: #f8840d;
-}
-
-/* Right Part */
-.right__part {
-	text-align: left;
-}
-
-.Information {
-	margin-bottom: 7px;
-}
-
-.company_info {
-	font-size: 16px;
-	line-height: 150%;
-	color: #30203d;
-	margin-bottom: 10px;
+@media screen and (min-width: 1123px) {
+    .contacts {
+        display: inline-flex;
+        position: relative;
+        right: -19px;
+    }
+    .footer__main {
+        text-align: center;
+        margin-bottom: 144px;
+    }
+    .block__content {
+        justify-content: center;
+    }
+    .block {
+        margin: 0 35px;
+    }
 }
 </style>
